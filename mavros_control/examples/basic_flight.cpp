87dc7a0c7b6d8c74927cd50471850ab3c1cb69a7
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2023-10-17 13:15:28 -0400

    Cleanup the basic flight script

--- mavros_control/examples/basic_flight.cpp
+++ mavros_control/examples/basic_flight.cpp
@@ -7,16 +7,28 @@
 #include <iostream>
 
 int main(int argc, char **argv){
+    // Initialize the node
     ros::init(argc, argv, "controller");
+
+    // Initialize the controller
     ControllerBase controller;
+
+    // Takeoff
     if(!controller.takeoff())
         return 0;
     sleep(5);
+
+    // Move to a position
     controller.command_pos(4, 2, 2, 0);
     sleep(5);
+
+    // Hold a position
     if (!controller.hold_position())
         return 0;
     sleep(5);
+
+    // Land
     controller.land();
+    
     return 0;
 }
